0d8088d1da8ba501fe5774d4f75604f6c5a15008
Wilmar dos Santos <dswilmar@gmail.com>
2022-10-15 08:31:18 -0300

    Criando migration de usuarios

---
+++ app/Database/Migrations/2022-10-15-111708_CriaTabelaUsuarios.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CriaTabelaUsuarios extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'nome' => [
+                'type' => 'VARCHAR',
+                'constraint' => 128
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 240
+            ],
+            'senha_hash' => [
+                'type' => 'VARCHAR',
+                'constraint' => 240
+            ],
+            'recupera_senha_hash' => [
+                'type' => 'VARCHAR',
+                'constraint' => 80,
+                'null' => true,
+                'default' => null
+            ],
+            'recupera_senha_expira' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+            'imagem' => [
+                'type' => 'VARCHAR',
+                'constraint' => 240,
+                'null' => true,
+                'default' => null
+            ],
+            'ativo' => [
+                'type' => 'BOOLEAN',
+                'null' => false,
+                'default' => false
+            ],
+            'criado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+            'atualizado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+            'deletado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('email');
+        $this->forge->createTable('usuarios');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('usuarios');
+    }
+}
